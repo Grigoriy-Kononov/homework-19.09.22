3181808a69956a8f4327f122c504553b8312b9e4
Grigory <kononoff88@gmail.com>
2022-09-20 00:38:24 +0300

    homework end

---
+++ Task_10/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход трехзначное число
+// и на вывходе показывает вторую цифру этого числа
+
+
+Console.Clear();
+
+Console.Write("Введите трехзначное число: ");
+int a = int.Parse(Console.ReadLine());
+if (a<100 || a>999)
+{
+Console.WriteLine($"Число {a} не является трехзначным");
+}
+else
+{
+string b = Convert.ToString(a);
+Console.WriteLine($"Вторая цифра - {b[1]}");
+}
---
+++ Task_13/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа
+// или сообщает, что третьей цифры нет
+
+
+Console.Clear();
+
+Console.Write("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+if (a<100)
+{
+Console.WriteLine($"У числа {a} нет третьей цифры");
+}
+else
+{
+string b = Convert.ToString(a);
+Console.WriteLine($"Третья цифра - {b[2]}");
+}
+
+
